cd11062540b8330dc040a29a6d1e792da15c1626
Milena <milena.cod@gmail.com>
2024-09-25 21:02:36 -0300

    criando a pag Error

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 import App from './App.jsx'
 import Home from './routes/Home.jsx'
+import Error from './routes/Error.jsx'
 
 const router = createBrowserRouter([
   {
     
     path:'/',element:<App/>,
+    errorElement:<Error/>,
     
 
     
